1a49711de1c63043112fc750ceb1031a4241cf20
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-12-11 10:36:14 +0300

    Split the script and stylesheet into different files. Might fix the AC button later.

---
+++ script.js
@@ -0,0 +1,30 @@
+// Store the current input value
+let currentInput = "";
+
+function inputNumber(num) {
+  // Update the current input value
+  currentInput += num;
+
+  // Update the input field
+  document.getElementById('input').innerHTML = currentInput;
+}
+
+function inputOperator(operator) {
+  // Update the current input value
+  currentInput += operator;
+
+  // Update the input field
+  document.getElementById('input').innerHTML = currentInput;
+}
+
+function calculate() {
+  // Calculate the result
+  let result = eval(currentInput);
+  // Display the result
+  document.getElementById('output').innerHTML = result;
+}
+
+function clear() {
+  //Clears the input field
+  document.getElementById('input').value = '';
+}
