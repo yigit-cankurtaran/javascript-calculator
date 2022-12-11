772b2ab2602c181d99af5ebdbb2268389cbe2d5f
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2022-12-11 14:16:03 +0300

    Used prettier on the JS file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const calculatorScreen = document.getElementById('calculator-screen');
-const calculatorKeys = document.querySelector('.calculator-keys');
-const calculator = document.querySelector('.calculator');
+const calculatorScreen = document.getElementById("calculator-screen");
+const calculatorKeys = document.querySelector(".calculator-keys");
+const calculator = document.querySelector(".calculator");
 
 // initialize the calculator screen value to zero
-let screenValue = '0';
+let screenValue = "0";
 let hasDecimal = false;
-let firstOperand = '';
-let operator = '';
+let firstOperand = "";
+let operator = "";
 
 // add event listener for when a calculator key is clicked
-calculatorKeys.addEventListener('click', (event) => {
+calculatorKeys.addEventListener("click", (event) => {
   // get the key that was clicked
   const key = event.target;
 
   // if the key is not a button, return
-  if (!key.matches('button')) {
+  if (!key.matches("button")) {
     return;
   }
 
@@ -22,25 +22,30 @@ calculatorKeys.addEventListener('click', (event) => {
   const keyValue = key.textContent;
 
   // handle the different key types
-  if (keyValue === '+' || keyValue === '-' || keyValue === '*' || keyValue === '/') {
+  if (
+    keyValue === "+" ||
+    keyValue === "-" ||
+    keyValue === "*" ||
+    keyValue === "/"
+  ) {
     // if the key is an operator, update the first operand and operator variables
     firstOperand = screenValue;
     operator = keyValue;
     hasDecimal = false;
     screenValue = keyValue;
-  } else if (keyValue === '.') {
+  } else if (keyValue === ".") {
     // if the key is a decimal point, add it to the screen value if it doesn't already have one
     if (!hasDecimal) {
       screenValue += keyValue;
       hasDecimal = true;
     }
-  } else if (keyValue === '=') {
+  } else if (keyValue === "=") {
     // if the key is the equals sign, evaluate the expression and update the screen value with the result
     const result = eval(`${firstOperand} ${operator} ${screenValue}`);
     screenValue = String(result);
-    hasDecimal = String(result).includes('.');
-    firstOperand = '';
-    operator = '';
+    hasDecimal = String(result).includes(".");
+    firstOperand = "";
+    operator = "";
     // update the screen with the result of the expression
     if (calculatorScreen) {
       calculatorScreen.textContent = result;
